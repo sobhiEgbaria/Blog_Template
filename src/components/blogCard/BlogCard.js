268233d6121d5035453a60ed23b36cb574c63482
sobhiEgbaria <sobhim686@gmail.com>
2023-12-26 23:55:29 +0200

    add like share  message icons

--- src/components/blogCard/BlogCard.js
+++ src/components/blogCard/BlogCard.js
@@ -1,16 +1,42 @@
 import "./BlogCard.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export const BlogCard = ({ img, title, description, date, id }) => {
+  const [isLike, setIsLike] = useState(false);
+  const [isBookMark, setIsBookMark] = useState(false);
   const scrollWin = () => {
     window.scrollTo(0, 0);
+  };
+
+  const handelLikeClick = () => {
+    setIsLike(!isLike);
+  };
+  const handelBookMarkClick = () => {
+    setIsBookMark(!isBookMark);
   };
 
   return (
     <>
       <div className="card">
         <div className="content">
-          <p className="date">{date}</p>
+          <p className="date">
+            {isBookMark ? (
+              <i
+                onClick={handelBookMarkClick}
+                id="icon_bookmark"
+                class="fa-solid fa-bookmark"
+              ></i>
+            ) : (
+              <i
+                onClick={handelBookMarkClick}
+                id="icon_bookmark"
+                class="fa-regular fa-bookmark"
+              ></i>
+            )}
+
+            {date}
+          </p>
           <h3 className="title">{title}</h3>
           <p className="description">
             {description}
@@ -19,6 +45,25 @@ export const BlogCard = ({ img, title, description, date, id }) => {
                 &nbsp; Read More<i class="fa-solid fa-angles-right"></i>
               </Link>
             </span>
+            <div>
+              {isLike ? (
+                <i
+                  onClick={handelLikeClick}
+                  id="icon"
+                  class="fa-solid fa-heart"
+                  style={{ color: "red" }}
+                ></i>
+              ) : (
+                <i
+                  onClick={handelLikeClick}
+                  id="icon"
+                  class="fa-regular fa-heart"
+                ></i>
+              )}
+
+              <i id="icon" class="fa-regular fa-message"></i>
+              <i id="icon" class="fa-regular fa-paper-plane"></i>
+            </div>
           </p>
         </div>
         <img src={img} alt="Avatar" />
